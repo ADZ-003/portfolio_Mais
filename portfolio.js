4c1e0b1afcd8c2af3461c2425ae6deaf634b701d
ADZ-003 <muhammadadzkiya821@gmail.com>
2025-10-04 10:53:52 +0700

    main

--- portfolio.js
+++ portfolio.js
@@ -1,4 +1,4 @@
-document.querySelector("form").addEventListener("submit", function(e) {
-  e.preventDefault();
-  alert("Thank you for your message!");
-});
+// document.querySelector("form").addEventListener("submit", function(e) {
+//   e.preventDefault();
+//   alert("Thank you for your message!");
+// });
